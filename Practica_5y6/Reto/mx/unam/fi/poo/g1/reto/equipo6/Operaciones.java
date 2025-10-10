9996f1294a0653f7b71dc4a337f3e6aae58133b9
sxid <92390610+sxidhub@users.noreply.github.com>
2025-10-10 07:09:18 -0600

    Update Operaciones.java

--- Practica_5y6/Reto/mx/unam/fi/poo/g1/reto/equipo6/Operaciones.java
+++ Practica_5y6/Reto/mx/unam/fi/poo/g1/reto/equipo6/Operaciones.java
@@ -5,19 +5,19 @@ public class Operaciones {
         }
 
         public double sumar(double a, double b) {
-            return Math.abs(a) + Math.abs(b);
+            return a + b;
         }
 
         public double restar(double a, double b) {
-            return Math.abs(a) - Math.abs(b);
+            return a - b;
         }
 
         public double multiplicar(double a, double b) {
-            return Math.abs(a) * Math.abs(b);
+            return a * b;
         }
 
         public double dividir(double a, double b) {
-            return Math.abs(a) / Math.abs(b);
+            return a / b;
         }
 
 }
